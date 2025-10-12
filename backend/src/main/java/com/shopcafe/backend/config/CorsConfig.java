cbb7b18039a02dc7ddf2f3326179786d089188a3
MinhNguyet-Love <minhnguyet22072004@gmail.com>
2025-10-13 00:41:05 +0700

    backend

--- backend/src/main/java/com/shopcafe/backend/config/CorsConfig.java
+++ backend/src/main/java/com/shopcafe/backend/config/CorsConfig.java
@@ -3,19 +3,22 @@ package com.shopcafe.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class CorsConfig {
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration cfg = new CorsConfiguration();
-        cfg.addAllowedOriginPattern("*");
-        cfg.addAllowedHeader("*");
-        cfg.addAllowedMethod("*");
-        cfg.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", cfg);
-        return source;
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
