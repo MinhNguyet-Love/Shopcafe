186509f9fc9e9e3bd86850f5d1d66f1dc0a0f61f
MinhNguyet-Love <minhnguyet22072004@gmail.com>
2025-10-19 00:44:45 +0700

    user

--- backend/src/main/java/com/shopcafe/backend/controller/UserController.java
+++ backend/src/main/java/com/shopcafe/backend/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "http://localhost:5176")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     private final UserService service;
